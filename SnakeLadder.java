50c5a7ef1c939852e19acf0bea0f2f899dfebd75
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-14 16:33:20 +0530

    ADD:Player Rolls the Dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,13 @@
+import java.util.Random;
 class SnakeLadder
 {
 public static void main(String args[])
 {
 System.out.println("Welcome to Snake ladder game");
 int Start_Position=0;
+Random rand=new Random();
+int dice=rand.nextInt(6);
+
+System.out.println("The player DiceRoll="+dice);
 }
 }
