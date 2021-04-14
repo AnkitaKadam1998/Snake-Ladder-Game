6e09c3a8b6c4fcda577e5c77133fb903f14b55bd
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-14 19:36:57 +0530

    ADD:Player won game

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,55 +1,111 @@
 import java.util.Random;
-class SnakeLadder
-{
-public static void main(String args[])
-{
-System.out.println("Welcome to Snake ladder game");
-int Start_Position=0;
-int PlayerPosition=0;
-int PlayerPeriviousPosition=0;
-int dicecount=0;
 
-while (PlayerPosition <= 100)
+public class SnakeLadder
 {
-Random rand=new Random();
-int dice=(rand.nextInt(6))+1;
+	public static void main(String args[])
+	{	
+	
+		System.out.println("Welcome to snake and ladder game!");
+		
+		
+		int player1StartPoint = 0, player2StartPoint = 0;
 
-Random ran=new Random();
-int playcheck=ran.nextInt(3);
+		int player1Position = 0, player2Position = 0, player1PreviousPoint = 0, player2PreviousPoint = 0, dice1Count = 0, dice2Count = 0;
 
-switch(playcheck)
-{
-case 0:
-			PlayerPosition = PlayerPosition;
-			System.out.println("You can't play.");
-			break;
-case 1:
-			PlayerPosition = dice + PlayerPosition;
-			System.out.println("You got ladder,you moved by " +dice+ "position");
-			break;
-case 2:
-			PlayerPosition = PlayerPosition - dice;
-			System.out.println("ooo You are bitten by snake");
-			break;
-default:
-			System.out.println("Invalid option");
-System.out.println("Player position after dice roll" + PlayerPosition); 
-}
-if(PlayerPosition < 0)
-{
-		PlayerPosition=Start_Position;
-}
-else if(PlayerPosition > 100)
-{
-		PlayerPosition=PlayerPeriviousPosition;
-}
-else if(PlayerPosition == 100)
-{
-		break;
-}
-}
-System.out.println("No of times dice has been rolled="+dicecount);
-System.out.println("Player wins");
-}
+		
+		while ((player1Position <= 100) && (player2Position <= 100)) {
+
+			
+      	Random rand1 = new Random();
+      	int dice1Roll = (rand1.nextInt(6))+1;
+			dice1Count = dice1Count + 1;
+
+			
+         Random rand2 = new Random();
+         int dice2Roll = (rand2.nextInt(6))+1;
+         dice2Count = dice2Count + 1;
+
+      	Random ran1 = new Random();
+      	int playCheck1 = ran1.nextInt(3);
+
+         Random ran2 = new Random();
+         int playCheck2 = ran2.nextInt(3);
+	
+			player1PreviousPoint = player1Position;
+			player2PreviousPoint = player2Position;
+			 
+			switch (playCheck1) {
+				case 0:
+					player1Position = player1Position;
+					break;
+
+				case 1:
+					player1Position = dice1Roll + player1Position;
+					
+					Random random1 = new Random();
+         		int extraDice1 = (random1.nextInt(6))+1;
+         		dice1Count = dice1Count + 1;
+				
+					player1Position = player1Position + extraDice1; 				
+
+					break;
+			
+				case 2:
+					player1Position = player1Position - dice1Roll;
+					break;
+			
+				default:
+					System.out.println("Invalid Option");
+
+			}
+         switch (playCheck2) {
+            case 0:
+               player2Position = player2Position;
+               break;
+
+            case 1:
+               player2Position = dice2Roll + player2Position;
+      			
+					Random random2 = new Random();
+               int extraDice2 = (random2.nextInt(6))+1;
+               dice2Count = dice2Count + 1;
+
+               player2Position = player2Position + extraDice2;         
+		
+					break;
+
+            case 2:
+               player2Position = player2Position - dice2Roll;
+               break;
+
+            default:
+               System.out.println("Invalid Option");
+
+         }
+								
+			if (player1Position < 0){
+				player1Position = player1StartPoint;
+			}else if (player1Position > 100){
+				player1Position = player1Position - player1PreviousPoint;
+			}else if (player1Position == 100){
+				break;
+			}
+
+			if (player2Position < 0){
+            player2Position = player2StartPoint;
+         }else if (player2Position > 100){
+            player2Position = player2Position - player2PreviousPoint;
+         }else if (player2Position == 100){
+            break;
+         }
+
+		}
+	
+		if(player1Position == 100){
+		System.out.println("Player 1 wins!, Game Over.");
+		} else if (player2Position == 100){
+		System.out.println("Player 2 wins!, Gamer Over.");
+		}
+	}
 }
 
